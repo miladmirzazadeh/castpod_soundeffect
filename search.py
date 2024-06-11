90c17a631ddcda2c96a1a5e6c29e0e928a8c2afb
Milad <milad.mirzazadeh@yahoo.com>
2024-06-11 14:08:35 +0000

    api key modified

--- search.py
+++ search.py
@@ -1,75 +1,3 @@
-# import pandas as pd
-# import time
-# import pandas as pd
-# import faiss
-# import numpy as np
-# import glob
-# import re
-# from openai import OpenAI
-# from dotenv import load_dotenv
-# from google.cloud import storage
-# from io import BytesIO
-# import io
-# import os
-
-
-# class SoundeffectRetriever():
-#     def __init__(self):
-#         self.bucket_name = "castpod-bucket"
-#         self.storage_client = storage.Client()
-#         self.openai_client = client = OpenAI(api_key ="sk-proj-n23q1OUCkTQbXSUDI1uHT3BlbkFJ4oHjBodFeVpWA5cDDYGU")
-#         self.faiss_index = self.download_faiss_index_to_memory()
-        
-#     def load_url_df(self, source_blob_name="soundeffects_urls.csv"):
-
-#         # Get the bucket
-#         bucket = self.storage_client.bucket(self.bucket_name)
-        
-#         # Get the blob
-#         blob = bucket.blob(source_blob_name)
-        
-#         # Download the blob to an in-memory bytes buffer
-#         csv_buffer = io.BytesIO()
-#         blob.download_to_file(csv_buffer)
-#         csv_buffer.seek(0)  # Reset buffer position to the beginning
-        
-#         # Load the CSV data into a pandas DataFrame
-#         df = pd.read_csv(csv_buffer)
-#         return df
-    
-#     def get_embedding(self, query,  model="text-embedding-3-small"):
-#         return self.openai_client.embeddings.create(input = [query], model=model).data[0].embedding
-    
-#     def download_faiss_index_to_memory(self, source_blob_name="faiss_index.index"):
-#         # Get the bucket
-#         bucket = self.storage_client.bucket(self.bucket_name)
-#         # Get the blob
-#         blob = bucket.blob(source_blob_name) 
-#         # Download the blob to an in-memory bytes buffer
-#         index_buffer = io.BytesIO()
-#         blob.download_to_file(index_buffer)
-#         index_buffer.seek(0)  # Reset buffer position to the beginning   
-#         # Load the FAISS index from the in-memory buffer
-#         faiss_index = faiss.read_index(index_buffer)
-#         return faiss_index
-
-
-    
-#     def search_faiss_index(self, query_embedding, k=1):
-#         distances, indices = index.search(query_embedding, k)
-#         url_df = self.load_url_df()
-#         url_list = url_df['download_link'].tolist()
-#         results = [url_list[idx] for i, idx in enumerate(indices[0])]
-#         return results
-
-#     def return_soundeffects(self, query):
-#         query_embedding = self.get_embedding(query)
-#         results = self.search_faiss_index(query_embedding)
-#         soundeffect_link = results[0]
-#         return soundeffect_link
-
-
-
 from flask import Flask, request, jsonify
 import pandas as pd
 import faiss
@@ -91,7 +19,7 @@ class SoundeffectRetriever():
     def __init__(self):
         self.bucket_name = "castpod-bucket"
         self.storage_client = storage.Client()
-        self.openai_client = OpenAI(api_key="sk-proj-PARlRihvR0E4jtCC2XAMT3BlbkFJuuGFu5dqYUTXZQ93sowT")
+        self.openai_client = OpenAI(api_key="sk-proj-iscJnAI4hzgx4Bd7bXnjT3BlbkFJRp3MIL3k0YqsgQk5VZzJ")
         self.faiss_index = self.load_faiss_index_from_local()
         
     def load_url_df(self, local_file_path="/cache/soundeffects_urls.csv"):
